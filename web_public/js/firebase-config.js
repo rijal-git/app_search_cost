f990bb05e197038f152943482aee31e1243edbb5
Developer Gabut <rijalsaja73@gmail.com>
2025-12-10 08:52:30 +0700

    Update AndroidManifest label to SearchCost and update firebase config

--- web_public/js/firebase-config.js
+++ web_public/js/firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
     apiKey: 'AIzaSyAl48ZyesmtstYWloY3fbKvawf7TtqHcYA',
     authDomain: 'app-cost-deff0.firebaseapp.com',
     projectId: 'app-cost-deff0',
-    storageBucket: 'app-cost-deff0.firebasestorage.app',
+    storageBucket: 'app-cost-deff0.appspot.com',
     messagingSenderId: '598468362354',
     appId: '1:598468362354:web:40e7779416b7b22dbd42eb'
 };
